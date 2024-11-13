568d48d130b08817841ebc0781da66b123fbc560
Lance Loper <lanceloper@u.boisestate.edu>
2024-11-13 03:18:55 -0700

    Implemented major column and updates for personal branch

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
-// TODO: Add any JavaScript here to make your page fancy :)
+// Toggle modal visibility
+function toggleModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = modal.style.display === "block" ? "none" : "block";
+}
+
+// Close modal if user clicks outside of it
+window.onclick = function(event) {
+  const modals = document.getElementsByClassName("modal");
+  Array.from(modals).forEach(modal => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+};
